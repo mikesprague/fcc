f9fcdde9a07e56a60e82d5fa8a1421b4f2478eb9
Mike Sprague <mikesprague@gmail.com>
2016-02-26 14:59:33 -0500

    Rename file

--- personal-portfolio/assets/js/app.js
+++ personal-portfolio/assets/js/app.js
@@ -18,6 +18,6 @@ jQuery( document ).ready( function( $ ) {
       }, 1500, "easeInOutExpo" );
   });
 
-  $( "[data-toggle=\"tooltip\"]" ).tooltip();
+  $( "[data-toggle='tooltip']" ).tooltip();
 
 });

b24a684f6bb8070061b3939d98398f53fad355d0
Mike Sprague <mikesprague@gmail.com>
2016-03-16 19:22:08 -0400

    Add wikipedia viewer files

---
+++ wikipedia-viewer/assets/js/app.js
@@ -0,0 +1,97 @@
+var app = {
+
+  wikipediaEndpoint: "https://en.wikipedia.org/w/api.php",
+  searchSuffix: "?action=query&format=json&prop=extracts|pageimages&generator=search&utf8=1&exsentences=1&exlimit=max&exintro=1&explaintext=1&exsectionformat=plain&piprop=thumbnail|name|original&pithumbsize=200&pilimit=max&gsrnamespace=0&gsrwhat=text&gsrlimit=20&redirects=1&gsrsearch=",
+  apiUserAgent: "FccWikipediaViewer-MS/v0.5.0",
+
+
+  initSearchField: function() {
+    var currentSearchRequest = null;
+    $( ".search-field" ).on( "keyup", function(e) {
+      var searchQuery = encodeURIComponent( $.trim( $( this ).val() ));
+
+      if (( searchQuery !== "" ) && ( searchQuery.length > 1 )) {
+        var fullUrlToCall = app.wikipediaEndpoint + app.searchSuffix + searchQuery;
+
+        currentSearchRequest = $.ajax({
+          url: fullUrlToCall,
+          dataType: "jsonp",
+          type: "GET",
+          crossDomain: true,
+          headers: {
+            "Api-User-Agent": app.apiUserAgent,
+            "Access-Control-Allow-Origin": "*"
+          },
+          cache: true,
+          beforeSend : function()    {
+            if ( currentSearchRequest != null ) {
+              currentSearchRequest.abort();
+            }
+          },
+          success: function ( data ) {
+            try {
+              app.renderSearchResults( data.query.pages );
+            } catch (e) {
+              app.clearSearchResults();
+              app.noSearchResults();
+            };
+            currentSearchRequest = null;
+          },
+          error: function( error ) {
+            console.log( error );
+            currentSearchRequest = null;
+          }
+        });
+      } else {
+        app.clearSearchResults();
+      }
+
+    });
+    //$( ".search-field" ).focus();
+
+  },
+
+  renderSearchResults: function( results ) {
+    app.clearSearchResults();
+    var searchResultsArray = Object.keys( results ).map(
+      function( key ) {
+        return results[key]
+      }
+    );
+    for ( var i = 0; i < searchResultsArray.length; i++ ) {
+      var searchResultTemplate = $( "#template-search-result" ).html();
+      var searchResultString = Mustache.render( searchResultTemplate, searchResultsArray[i] );
+      $( ".search-results" ).append( searchResultString );
+    }
+    app.fixBrokenImages();
+  },
+
+  clearSearchResults: function() {
+    $( ".search-results" ).empty();
+  },
+
+  noSearchResults: function() {
+    $( ".search-results" ).html( "<h1>No Results: <small>Please try another search term</small></h1>" );
+  },
+
+  fixBrokenImages: function() {
+    $( "img" ).error( function() {
+      $( this ).attr( "src", "https://placehold.it/215?text=n/a" );
+    });
+  },
+
+  init: function() {
+
+    app.initSearchField();
+
+  }
+};
+
+
+
+
+jQuery( document ).ready( function( $ ) {
+
+  app.init();
+
+});
